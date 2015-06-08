3d99e96ff9a7ffe630be2afa113f0c3ed2ba427e
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-06-08 15:23:25 +0200

    Update tests to take into account a response stream piped to fs, wrote new tests for handling errors

--- test/downloaderTest.js
+++ test/downloaderTest.js
@@ -13,15 +13,21 @@ var dl          = require('../lib/downloader');
 // Fixtures
 var dummyJquery = require('./helpers/fixtures').dummyJquery();
 
+// var EventEmitter = require('events').EventEmitter;
+var PassThrough = require('stream').PassThrough;
+
 // Shut up colog
 colog.silent(true);
 
-describe ('downloader', function() {
+describe('downloader', function() {
 
   // Setup file system stubs, httprequest stubs
   beforeEach(function() {
     mock_fs();
-    sinon.stub(request, "get").yields(null, {statusCode: 200}, 'someData');
+    // sinon.stub(request, "get").yields(null, {statusCode: 200}, 'someData');
+    this.responseStream = new PassThrough();
+    this.responseStream.write('RESPONSE DATA');
+    sinon.stub(request, "get").returns(this.responseStream);
   });
   afterEach(function() {
     mock_fs.restore();
@@ -83,42 +89,124 @@ describe ('downloader', function() {
 
     }); // End of calls api.CDNJS
 
-    describe('writes files to disk', function() {
+    describe('interacts with the file system', function() {
 
-      it('saves all files to disk', function() {
-        dl.download(dummyJquery, undefined, "");
-        var expected = ["file1-4.4.4.js", "file2-4.4.4.js"];
-        expect(fs.readdirSync("")).to.eql(expected);
-      });
+      describe('on#response', function() {
 
-      it('creates dirs if necessary', function() {
-        dl.download(dummyJquery, "4.1.1", "");
-        expect(fs.readdirSync("")).to.eql(["css", "js"]);
-        expect(fs.readdirSync("js")).to.eql(["file1-4.1.1.js"]);
-        expect(fs.readdirSync("css")).to.eql(["file2-4.1.1.js"]);
-      });
+        context('200 OK', function() {
 
-      it('creates all files in specified output dir', function() {
-        dl.download(dummyJquery, undefined, "someDir");
-        expect(fs.readdirSync("")).to.eql(["someDir"]);
-        var expected = ["file1-4.4.4.js", "file2-4.4.4.js"];
-        expect(fs.readdirSync("someDir")).to.eql(expected);
-      });
+          it('saves all files to disk', function() {
+            dl.download(dummyJquery, undefined, "");
+            this.responseStream.emit('response', {statusCode: 200});
+            var expected = ["file1-4.4.4.js", "file2-4.4.4.js"];
+            expect(fs.readdirSync("")).to.eql(expected);
+          });
 
-      it('creates all folders recursively', function() {
-        dl.download(dummyJquery, "4.1.1", "someDir");
-        expect(fs.readdirSync("")).to.eql(["someDir"]);
-        var expected = ["css", "js"];
-        expect(fs.readdirSync("someDir")).to.eql(expected);
-      });
+          it('creates dirs if necessary', function() {
+            dl.download(dummyJquery, "4.1.1", "");
+            this.responseStream.emit('response', {statusCode: 200});
+            expect(fs.readdirSync("")).to.eql(["css", "js"]);
+            expect(fs.readdirSync("js")).to.eql(["file1-4.1.1.js"]);
+            expect(fs.readdirSync("css")).to.eql(["file2-4.1.1.js"]);
+          });
 
-      it('saves the one file to disk when minimal is specified', function() {
-        dl.download(dummyJquery, "4.1.1", "", true);
-        expect(fs.readdirSync("")).to.eql(["latest.js"]);
-      })
+          it('creates all files in specified output dir', function() {
+            dl.download(dummyJquery, undefined, "someDir");
+            this.responseStream.emit('response', {statusCode: 200});
+            expect(fs.readdirSync("")).to.eql(["someDir"]);
+            var expected = ["file1-4.4.4.js", "file2-4.4.4.js"];
+            expect(fs.readdirSync("someDir")).to.eql(expected);
+          });
 
-    });
+          it('creates all folders recursively', function() {
+            dl.download(dummyJquery, "4.1.1", "someDir");
+            this.responseStream.emit('response', {statusCode: 200});
+            expect(fs.readdirSync("")).to.eql(["someDir"]);
+            var expected = ["css", "js"];
+            expect(fs.readdirSync("someDir")).to.eql(expected);
+          });
 
-  });
+          it('saves the one file to disk when minimal is specified', function() {
+            dl.download(dummyJquery, "4.1.1", "", true);
+            this.responseStream.emit('response', {statusCode: 200});
+            expect(fs.readdirSync("")).to.eql(["latest.js"]);
+          });
 
-});
+        }); // End of 200 OK
+
+        context('404 NOT FOUND', function() {
+
+          before(function() {
+            sinon.stub(console, "error");
+          });
+          after(function() {
+            console.error.restore();
+          });
+
+          it('doesn\'t create the files', function() {
+            dl.download(dummyJquery, "4.1.1", "");
+            this.responseStream.emit('response', {statusCode: 404});
+            expect(fs.readdirSync("")).to.be.empty;
+          });
+
+          it('doesn\'t create the given outputDir', function() {
+            dl.download(dummyJquery, "", "someDir");
+            this.responseStream.emit('response', {statusCode: 404});
+            expect(fs.existsSync("someDir")).to.be.false;
+          });
+
+          it('doesn\'t create dirs for the downloaded files', function() {
+            dl.download(dummyJquery, "4.1.1", "");
+            this.responseStream.emit('response', {statusCode: 404});
+            expect(fs.readdirSync("")).to.not.eql(["css", "js"]);
+          });
+
+          it('prints out the error', function() {
+            dl.download(dummyJquery, "4.1.1", "");
+            this.responseStream.emit('response', {statusCode: 404,
+                                                  statusMessage: "not found"});
+            var expectedErr1 = "404 not found - js/file1-4.1.1.js";
+            var expectedErr2 = "404 not found - css/file2-4.1.1.js";
+            expect(console.error).to.have.been.calledWith(expectedErr1);
+            expect(console.error).to.have.been.calledWith(expectedErr2);
+          });
+
+        });
+
+      }); // End of on#response
+
+      describe("on#error", function() {
+
+        it('doesn\'t create the files', function() {
+          dl.download(dummyJquery, "4.1.1", "");
+          this.responseStream.emit('error');
+          expect(fs.readdirSync("")).to.be.empty;
+        });
+
+        it('doesn\'t create the given outputDir', function() {
+          dl.download(dummyJquery, "", "someDir");
+          this.responseStream.emit('error');
+          expect(fs.existsSync("someDir")).to.be.false;
+        });
+
+        it('doesn\'t create dirs for the downloaded files', function() {
+          dl.download(dummyJquery, "4.1.1", "");
+          this.responseStream.emit('error');
+          expect(fs.readdirSync("")).to.not.eql(["css", "js"]);
+        });
+
+        it('prints out the error', function() {
+          sinon.stub(console, "error");
+          dl.download(dummyJquery, "4.1.1", "");
+          this.responseStream.emit('error', "ERRORMESSAGE");
+          expect(console.error).to.have.been.calledWith("ERRORMESSAGE");
+          console.error.restore();
+        });
+
+      }); // End of on#error
+
+    }); // End of interacts with the file system
+
+  }); // End of .download()
+
+}); // End of downloader
